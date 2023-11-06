6c040ca6febab0e76249f31b70cb02fed4931e3a
takuyakawata <kawatatakuya@tk2023.local>
2023-11-07 00:58:40 +0900

    test3

--- programming/react_app/src/pages/Reserve.jsx
+++ programming/react_app/src/pages/Reserve.jsx
@@ -18,13 +18,14 @@ export const Reserve = () => {
   return (
     <>
       <p>授業予約の画面</p>
+      <p>お探しの授業がすぐに見つかる！！</p>
       <input 
         type="text" 
         value={classReserve} 
         onChange={(e) => setClassReserve(e.target.value)} 
         placeholder="授業名を入力してください"
       />
-      <ActionButton text="予約する" action={handleReserve} />
+      <ActionButton text="予約する" action={handleReserve} className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"/>
       <p>{classReserveResult}</p>
     </>
   );
